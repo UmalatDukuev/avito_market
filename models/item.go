95185e00956f688c15470a4c5c3988784915f597
Umalat <dukuev037@mail.ru>
2025-02-16 22:03:54 +0300

    основные эндпоинты реализованы

--- models/item.go
+++ models/item.go
@@ -2,7 +2,6 @@ package models
 
 type Item struct {
 	ID          int    `json:"id" db:"id"`
-	Type        string `json:"type" db:"type"`
 	Name        string `json:"name" db:"name"`
 	Description string `json:"description,omitempty" db:"description"`
 	Quantity    int    `json:"quantity" db:"quantity"`
--- schema/migrations/000003_create_items_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE items (
-    id serial not null unique,
-    name varchar(255) not null,
-    description text,
-    price int not null,
-    -- inventory_count int default 0,
-    PRIMARY KEY (id)
-);
